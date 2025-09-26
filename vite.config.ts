56a3fd7373d15b9a14d02a23f9d7c670d73a81ba
zimzim2255 <heyyheyy299@gmail.com>
2025-09-26 20:16:04 +0100

    Fix base path to match actual repository name (roulling-car)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rouling-car-seit-2/',
+  base: '/roulling-car/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
